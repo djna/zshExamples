1fd6a4c4c2d59861431dee5772832b1977a03496
David Artus <djna01@gmail.com>
2022-06-30 14:44:15 +0000

    loop through matches and groups

--- apply.py
+++ apply.py
@@ -1,6 +1,18 @@
 
 import re
 
-txt = "The rain in Spain"
-x = re.findall("ai", txt)
-print(x)
+regex = re.compile(r"(?P<head>\d)(?P<tail>\d+)")
+
+filename = "input.txt"
+with open(filename) as file:
+    for line in file:
+        print("line: ", line.rstrip())
+        for match in re.finditer(regex, line):
+            print("match:", match)
+            print("groups:", match.groups() )
+            print("head:", match.group('head') )
+            print("tail:", match.group('tail') )
+            
+
+file.close()
+
