4873466b5e356ab495d05367644875e3e3672577
David Artus <djna01@gmail.com>
2022-06-30 08:20:57 +0000

    add regex

---
+++ apply.js
@@ -0,0 +1,24 @@
+
+const fs = require('fs');
+const readline = require('readline');
+
+async function processLineByLine() {
+  const fileStream = fs.createReadStream('input.txt');
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+  // Note: we use the crlfDelay option to recognize all instances of CR LF
+  // ('\r\n') in input.txt as a single line break.
+
+  let regex = /\d+/;
+  for await (const line of rl) {
+    // Each line in input.txt will be successively available here as `line`.
+    if ( regex.match($line)){
+        console.log(`${line} matches ${regex.source}`);
+    }
+  }
+}
+
+processLineByLine();
